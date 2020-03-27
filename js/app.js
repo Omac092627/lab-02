6a03c0fbb7a99f1097e40d5fd850fb68f67f7cf0
Lami Beach <oogunde0413@gmail.com>
2020-03-26 18:15:34 -0700

    not feeling well today, acping early

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 'use strict'
 
+let infoArray = [];
+
+function JsonStuff (image_url, title, description, keyword, horns){
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    infoArray.push(this);
+};
+
+$(document).ready(function(){
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
+    
+    
+    $.ajax('page-1.json', ajaxSettings)
+
+    
+});
+
